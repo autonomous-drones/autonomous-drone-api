970178b8c92367de7a92b5c513b4504cbb0ae4dc
Fojtík,Honza J <h.fojtik@student.fontys.nl>
2021-11-16 14:10:19 +0100

    updated method for downloading multiple files

--- app/main.py
+++ app/main.py
@@ -28,26 +28,6 @@ def authorize(credentials: HTTPBasicCredentials = Depends(security)):
     return credentials.username
 
 
-def zipfiles(filenames):
-    zip_filename = "archive.zip"
-
-    s = io.BytesIO()
-    zf = zipfile.ZipFile(s, "w")
-
-    for fpath in filenames:
-        fdir, fname = os.path.split(fpath)
-
-        zf.write(fpath, fname)
-
-    zf.close()
-
-    resp = Response(s.getvalue(), media_type="application/x-zip-compressed", headers={
-        'Content-Disposition': f'attachment;filename={zip_filename}'
-    })
-
-    return resp
-
-
 @app.get("/")
 async def root():
     return {"message": "Autonomous Drone API"}
@@ -97,3 +77,35 @@ async def read_items(flightName: str, imageId: int, credentials: HTTPBasicCreden
         filename=f"{imageId}_cam-image.jpg",
         media_type='image/jpeg'
     )
+
+
+def zipfiles(filenames):
+    zip_filename = "archive.zip"
+
+    s = io.BytesIO()
+    zf = zipfile.ZipFile(s, "w")
+
+    for fpath in filenames:
+        # Calculate path for file in zip
+        fdir, fname = os.path.split(fpath)
+
+        # Add file, at correct path
+        zf.write(fpath, fname)
+
+    # Must close zip for all contents to be written
+    zf.close()
+
+    # Grab ZIP file from in-memory, make response with correct MIME-type
+    resp = Response(s.getvalue(), media_type="application/x-zip-compressed", headers={
+        'Content-Disposition': f'attachment;filename={zip_filename}'
+    })
+
+    return resp
+
+
+@app.get("/image_from_id/")
+async def image_from_id(image_id: int):
+
+    # Get image from the database
+    img = ...
+    return zipfiles(img)
